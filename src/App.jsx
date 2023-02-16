edd30a46735be21d60580b3c214e73748cecfaa1
gokrusk <nigelljama@gmail.com>
2023-02-15 20:23:38 -0500

    rendering buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,73 @@
 import { useState } from 'react';
+import { evaluate } from 'mathjs';
 import { Screen } from './components/Screen';
 import { ClearButton } from './components/ClearButton';
 import Button from './components/Button';
 import espochLogo from './images/logoico.png';
-import './styles/ClearButton.css'
-import './styles/Button.css'
-import './styles/Screen.css'
+import './styles/ClearButton.css';
+import './styles/Button.css';
+import './styles/Screen.css';
+
+let a = [],
+  cont = 0,
+  cont2 = 0,
+  b = ['*', '+', '/', '-'],
+  c = [];
 
 function App() {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
 
-  const agregarInput = val =>{
-    setInput(input + val)
-  }
+  const isOperator = (valor) => {
+    return isNaN(valor) && valor !== '.' && valor !== '=';
+  };
+
+  const listaComprobar = (a, b) => {
+    let cont = 0;
+    a.forEach((element) => {
+      if (element === b) {
+        cont++;
+        console.log(cont);
+      }
+    });
+    return cont >= 1;
+  };
+
+  const agregarInput = (val) => {
+    if (cont > 0) {
+      if (isOperator(val)) {
+        console.log(c);
+        a.pop() === val
+          ? alert('Símbolos iguales seguidos')
+          : isOperator(c.pop())
+          ? alert('Símbolos distintos seguidos')
+          : setInput(input + val);
+        a.push(val);
+      } else {
+        setInput(input + val);
+        a.push(val);
+        cont++;
+      }
+    } else {
+      if (val !== '*' && val !== '/') {
+        a.push(val);
+        setInput(input + val);
+        cont++;
+      } else {
+        alert('No iniciar con * o /');
+      }
+    }
+    c = [...a];
+    console.log(c);
+  };
+
+  const calcular = () => {
+    if (input) {
+      //null: true
+      setInput(evaluate(input));
+    } else {
+      alert('Ingrese valores');
+    }
+  };
 
   return (
     <div className='App'>
@@ -20,33 +75,41 @@ function App() {
         <img className='espochLogo' src={espochLogo} alt='Logo ESPOCH' />
       </div>
       <div className='cont-calculator'>
-        <Screen input={input}/>
+        <Screen input={input} />
         <div className='fila'>
-          <Button manageClick = {agregarInput}>1</Button>
-          <Button manageClick = {agregarInput}>2</Button>
-          <Button manageClick = {agregarInput}>3</Button>
-          <Button manageClick = {agregarInput}>+</Button>
+          <Button manageClick={agregarInput}>1</Button>
+          <Button manageClick={agregarInput}>2</Button>
+          <Button manageClick={agregarInput}>3</Button>
+          <Button manageClick={agregarInput}>+</Button>
         </div>
         <div className='fila'>
-          <Button manageClick = {agregarInput}>4</Button>
-          <Button manageClick = {agregarInput}>5</Button>
-          <Button manageClick = {agregarInput}>6</Button>
-          <Button manageClick = {agregarInput}>-</Button>
+          <Button manageClick={agregarInput}>4</Button>
+          <Button manageClick={agregarInput}>5</Button>
+          <Button manageClick={agregarInput}>6</Button>
+          <Button manageClick={agregarInput}>-</Button>
         </div>
         <div className='fila'>
-          <Button manageClick = {agregarInput}>7</Button>
-          <Button manageClick = {agregarInput}>8</Button>
-          <Button manageClick = {agregarInput}>9</Button>
-          <Button manageClick = {agregarInput}>*</Button>
+          <Button manageClick={agregarInput}>7</Button>
+          <Button manageClick={agregarInput}>8</Button>
+          <Button manageClick={agregarInput}>9</Button>
+          <Button manageClick={agregarInput}>*</Button>
         </div>
         <div className='fila'>
-          <Button manageClick = {agregarInput}>=</Button>
-          <Button manageClick = {agregarInput}>0</Button>
-          <Button manageClick = {agregarInput}>.</Button>
-          <Button manageClick = {agregarInput}>/</Button>
+          <Button manageClick={calcular}>=</Button>
+          <Button manageClick={agregarInput}>0</Button>
+          <Button manageClick={agregarInput}>.</Button>
+          <Button manageClick={agregarInput}>/</Button>
         </div>
         <div className='fila'>
-          <ClearButton manageClick = {() => setInput('')}>CLEAR</ClearButton>
+          <ClearButton
+            manageClick={() => {
+              setInput('');
+              a = [];
+              cont = 0;
+            }}
+          >
+            CLEAR
+          </ClearButton>
         </div>
       </div>
     </div>
--- src/components/ClearButton.jsx
+++ src/components/ClearButton.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-export const ClearButton = ({children,manageClick}) =>(
-    <div 
-        className='clear-button'
-        onClick={manageClick}
-    >
-      {children}
-    </div>
-)
+export const ClearButton = ({ children, manageClick }) => (
+  <div className='clear-button' onClick={manageClick}>
+    {children}
+  </div>
+);
